66196026f7eb0e801534c4d1e1a9785b0ea78369
Jeffrey Palermo <jeffrey@clear-measure.com>
2017-11-10 23:46:42 -0600

    Revert "First EF Core 2 mapping added alongside NHibernate - proving compatibility for the base Employee class."
    This reverts commit 052a4ff62db499c3de9f1667733c2b58e5276ddd.

--- default.ps1
+++ default.ps1
@@ -76,7 +76,7 @@ task InjectConnectionString {
 
 task Compile -depends Init {
     exec {
-        & msbuild /t:Clean`;Rebuild /v:d /maxcpucount:1 /nologo /p:Configuration=$projectConfig /p:OctoPackPackageVersion=$version /p:RunOctoPack=$runOctoPack /p:OctoPackEnforceAddingFiles=true $source_dir\$projectName.sln
+        & msbuild /t:Clean`;Rebuild /v:m /maxcpucount:1 /nologo /p:Configuration=$projectConfig /p:OctoPackPackageVersion=$version /p:RunOctoPack=$runOctoPack /p:OctoPackEnforceAddingFiles=true $source_dir\$projectName.sln
     }
 
 	Copy_and_flatten $source_dir *.nupkg $build_dir
