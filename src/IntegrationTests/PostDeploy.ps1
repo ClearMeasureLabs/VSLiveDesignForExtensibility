5293016cca1cf35b6d2a47d02c23f2b3c7613432
Jeffrey Palermo <jeffrey@clear-measure.com>
2017-12-08 14:51:54 -0600

    #3963 Converted powershell environment configuration scripts to use new connection string file - needed by EFCore - was using old NHibernate config file

--- src/IntegrationTests/PostDeploy.ps1
+++ src/IntegrationTests/PostDeploy.ps1
@@ -2,36 +2,30 @@
 # PostDeploy.ps1
 #
 $path = $PSScriptRoot
-$hibernateConfig = "$path\hibernate.cfg.xml"
+$configFile = "$path\ConnectionStrings.config"
 $integratedSecurity = "Integrated Security=true"
 $DatabaseServer = $OctopusParameters["DatabaseServer"]
 $DatabaseName = $OctopusParameters["DatabaseName"]
 $connection_string = "server=$DatabaseServer;database=$DatabaseName;$integratedSecurity;"
 
-$filePath = $hibernateConfig
-$xpath = "//e:property[@name = 'connection.connection_string']"
-$value = $connection_string
-$namespaces = @{"e" = "urn:nhibernate-configuration-2.2"}
+$xpath = "//add[@name='Bootcamp']/@connectionString"
 
-
-[xml] $fileXml = Get-Content $filePath
+poke-xml $configFile $xpath $connection_string
     
-if($namespaces -ne $null -and $namespaces.Count -gt 0) {
-    $ns = New-Object Xml.XmlNamespaceManager $fileXml.NameTable
-    $namespaces.GetEnumerator() | %{ $ns.AddNamespace($_.Key,$_.Value) }
-    $node = $fileXml.SelectSingleNode($xpath,$ns)
-} else {
+function script:poke-xml($filePath, $xpath, $value) {
+    [xml] $fileXml = Get-Content $filePath
     $node = $fileXml.SelectSingleNode($xpath)
-}
     
-if($node.NodeType -eq "Element") {
-    $node.InnerText = $value
-} else {
-    $node.Value = $value
-}
+    Assert ($node -ne $null) "could not find node @ $xpath"
+        
+    if($node.NodeType -eq "Element") {
+        $node.InnerText = $value
+    } else {
+        $node.Value = $value
+    }
 
-$fileXml.Save($filePath) 
-
+    $fileXml.Save($filePath) 
+} 
 Write-Host "DatabaseServer: $DatabaseServer"
 Write-Host "DatabaseName: $DatabaseName"
 Write-Host "-----------------------"
--- src/SmokeTests/AzureDataLoader.ps1
+++ src/SmokeTests/AzureDataLoader.ps1
@@ -2,37 +2,31 @@
 # AzureDataLoader.ps1
 #
 $path = $PSScriptRoot
-$hibernateConfig = "$path\hibernate.cfg.xml"
+$configFile = "$path\ConnectionStrings.config"
 $databaseName = $env:DatabaseName
 $databaseServer = $env:DatabaseServer
 $databaseUser = $env:DatabaseUser
 $databasePassword = $env:DatabasePassword
 $connection_string = "Server=tcp:$databaseServer,1433;Initial Catalog=$databaseName;Persist Security Info=False;User ID=$databaseUser;Password=$databasePassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 
-$filePath = $hibernateConfig
-$xpath = "//e:property[@name = 'connection.connection_string']"
-$value = $connection_string
-$namespaces = @{"e" = "urn:nhibernate-configuration-2.2"}
+$xpath = "//add[@name='Bootcamp']/@connectionString"
 
-
-[xml] $fileXml = Get-Content $filePath
+poke-xml $configFile $xpath $connection_string
     
-if($namespaces -ne $null -and $namespaces.Count -gt 0) {
-    $ns = New-Object Xml.XmlNamespaceManager $fileXml.NameTable
-    $namespaces.GetEnumerator() | %{ $ns.AddNamespace($_.Key,$_.Value) }
-    $node = $fileXml.SelectSingleNode($xpath,$ns)
-} else {
+function script:poke-xml($filePath, $xpath, $value) {
+    [xml] $fileXml = Get-Content $filePath
     $node = $fileXml.SelectSingleNode($xpath)
-}
     
-if($node.NodeType -eq "Element") {
-    $node.InnerText = $value
-} else {
-    $node.Value = $value
-}
+    Assert ($node -ne $null) "could not find node @ $xpath"
+        
+    if($node.NodeType -eq "Element") {
+        $node.InnerText = $value
+    } else {
+        $node.Value = $value
+    }
 
-$fileXml.Save($filePath) 
-
+    $fileXml.Save($filePath) 
+} 
 Write-Host "DatabaseServer: $DatabaseServer"
 Write-Host "DatabaseName: $DatabaseName"
 Write-Host "-----------------------"
