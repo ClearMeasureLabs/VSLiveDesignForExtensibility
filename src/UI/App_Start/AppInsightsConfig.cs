82788f1e2fe323fd9345f36304fe12c38d86a9b9
Kyle Nunery <kyle.nunery@gmail.com>
2018-01-08 12:23:45 -0500

    set application version and environment for app insights

--- src/UI/App_Start/AppInsightsConfig.cs
+++ src/UI/App_Start/AppInsightsConfig.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using System.Configuration;
+﻿using System.Configuration;
+using ClearMeasure.Bootcamp.Core.Services;
+using ClearMeasure.Bootcamp.Core.Services.Impl;
+using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
 
 namespace ClearMeasure.Bootcamp.UI
@@ -9,6 +11,19 @@ namespace ClearMeasure.Bootcamp.UI
         public static void Configure()
         {
             TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new TelemetryInitializer());
+        }
+
+        public class TelemetryInitializer : ITelemetryInitializer
+        {
+            public void Initialize(ITelemetry telemetry)
+            {
+                IApplicationInformation appInfo = new ApplicationInformation();
+                // Component.Version maps to application_Version
+                telemetry.Context.Component.Version = $"{appInfo.ProductName}-{appInfo.ProductVersion}";
+                // Environment will be mapped to customDimensions
+                telemetry.Context.Properties.Add("Environment", ConfigurationManager.AppSettings["SiteEnvironment"]);
+            }
         }
     }
 }
